c7e927b2318110f74042d4eb7f9355ce049c3d7e
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-02-03 19:12:53 -0300

    Configurando notificações de agendamentos

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '82e768acd26843',
+    pass: '57f84e875e871b',
+  },
+  default: {
+    from: 'GoBarber <npreply@gobarber.com>',
+  },
+};
