0cf2025f05c9394b1721d1b3d6e6f5a8792624a7
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-03-16 15:20:35 -0300

    Modificações no retorno de dados dos usuários

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ import path from 'path';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
+import cors from 'cors';
 
 // Iimportação de arquivos
 import routes from './routes';
@@ -45,6 +46,7 @@ class App {
   */
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -21,6 +22,13 @@ class SessionController {
 
     const user = await User.findOne({
       where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'name', 'url'],
+        },
+      ],
     });
 
     if (!user) {
@@ -35,13 +43,15 @@ class SessionController {
       });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
